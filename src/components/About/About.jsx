92b6cb6a17a0188c01288b75a411063f0de828e2
Atharv Rajmane <atharvrajmane81@gmail.com>
2025-09-19 16:18:39 +0530

    Initial commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -41,7 +41,7 @@ const About = () => {
             challenges that contribute to meaningful, real-world projects.
           </p>
           <a
-            href="https://drive.google.com/file/d/1e3ytnaxnjScQLr9FbB7M0pBuPa7rZVE_/view?usp=sharing"
+            href="https://drive.google.com/file/d/1cBuShyrievKUAUcJBmxGP8OxmIrITcb_/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
